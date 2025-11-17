14657a3c6e3661bd59d3f6e632c738fbe3a4826e
Hyelee Kim <112936202+hyel0000@users.noreply.github.com>
2025-11-17 18:05:32 +0900

    Update path_cfgs.py

--- MCAoAN/MCAoA/cfgs/path_cfgs.py
+++ MCAoAN/MCAoA/cfgs/path_cfgs.py
@@ -10,10 +10,10 @@ class PATH:
     def __init__(self):
 
         # vqav2 dataset root path
-        self.DATASET_PATH = "/home/seinkwon/hyelee/MCAN/datasets/vqa/"
+        self.DATASET_PATH = './datasets/vqa/'
 
         # bottom up features root path
-        self.FEATURE_PATH = '/scratch/seinkwon/hyelee/coco_extract/'
+        self.FEATURE_PATH = './datasets/coco_extract/'
 
         self.init_path()
 
@@ -29,13 +29,15 @@ class PATH:
         self.QUESTION_PATH = {
             'train': self.DATASET_PATH + 'v2_OpenEnded_mscoco_train2014_questions.json',
             'val': self.DATASET_PATH + 'v2_OpenEnded_mscoco_val2014_questions.json',
-            'test': self.DATASET_PATH + 'v2_OpenEnded_mscoco_test2015_questions.json'
+            'test': self.DATASET_PATH + 'v2_OpenEnded_mscoco_test2015_questions.json',
+            'vg': self.DATASET_PATH + 'VG_questions.json',
         }
 
         self.ANSWER_PATH = {
             'train': self.DATASET_PATH + 'v2_mscoco_train2014_annotations.json',
-            'val': self.DATASET_PATH + 'v2_mscoco_val2014_annotations.json'
-            }
+            'val': self.DATASET_PATH + 'v2_mscoco_val2014_annotations.json',
+            'vg': self.DATASET_PATH + 'VG_annotations.json',
+        }
 
         self.RESULT_PATH = './results/result_test/'
         self.PRED_PATH = './results/pred/'
@@ -79,4 +81,3 @@ class PATH:
 
         print('Finished')
         print('')
-
